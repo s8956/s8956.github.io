a835394306b4030edde9797acea0d9598a7de9a5
ghpbot <ghpbot@bk.ru>
2023-11-03 13:29:10 +0000

    deploy: site-0003/site@3a918c7929a03eb15702c0b9e1d7a449eb88b68e

---
+++ ru/drafts/2023/11/e06f1841-7479-50cc-a528-1aca56d91d50/auto.tpm.bat
@@ -0,0 +1,9 @@
+:: Bypassing system requirements checks (Windows 11).
+
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE" /v "BypassNRO" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassCPUCheck" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassRAMCheck" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassStorageCheck" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /f /t "REG_DWORD" /d "1"
+reg add "HKLM\SYSTEM\Setup\MoSetup" /v "AllowUpgradesWithUnsupportedTPMOrCPU" /f /t "REG_DWORD" /d "1"
