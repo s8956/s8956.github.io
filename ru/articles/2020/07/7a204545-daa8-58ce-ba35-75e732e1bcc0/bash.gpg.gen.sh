05f33e1b043b7952726c648f4883e3129ce02c13
ghpbot <ghpbot@bk.ru>
2023-11-26 12:29:04 +0000

    deploy: site-0003/site@6e03fb8c78a8a3654c0270b37f9c4ab3ac1973c2

---
+++ ru/articles/2020/07/7a204545-daa8-58ce-ba35-75e732e1bcc0/bash.gpg.gen.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash -e
+#
+# Creating a new GPG key.
+#
+# @package    Bash
+# @author     Kitsune Solar <mail@kitsune.solar>
+# @copyright  2023 Library Online
+# @license    MIT
+# @version    0.1.0
+# @link       https://lib.onl/ru/articles/2020/07/7a204545-daa8-58ce-ba35-75e732e1bcc0/
+# -------------------------------------------------------------------------------------------------------------------- #
+
+(( EUID == 0 )) && { echo >&2 'This script should not be run as root!'; exit 1; }
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# CREATING AND EXPORT KEY.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+key() {
+  name="${1}"
+  mail="${2}"
+  pass="${3}"
+
+  # Creating key.
+  gpg --batch --gen-key <<EOF
+Key-Type: EDDSA
+Key-Curve: ed25519
+Subkey-Type: ECDH
+Subkey-Curve: cv25519
+Name-Real: ${name}
+Name-Email: ${mail}
+Expire-Date: 0
+Passphrase: ${pass}
+%commit
+EOF
+
+  # Export public key.
+  gpg --batch --armor \
+    --output "${mail}.public.asc" \
+    --export "${mail}"
+
+  # Export private key.
+  gpg --batch --armor --pinentry-mode=loopback --yes \
+    --passphrase "${pass}" \
+    --output "${mail}.private.asc" \
+    --export-secret-keys "${mail}"
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------< RUNNING SCRIPT >------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+"$@"
