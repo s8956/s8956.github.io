8f9be6d4eab4c097fe3137ef4158a51d17a04d03
ghpbot <ghpbot@bk.ru>
2024-04-07 08:14:01 +0000

    deploy: site-0003/site@1ed5829f0a8c07a4a39ad3eaa5be8c779b3f88d1

--- ru/articles/2023/10/6733cb51-62a0-5ed9-b421-8f08c4e0cb18/bash.openssl.ca.sh
+++ ru/articles/2023/10/6733cb51-62a0-5ed9-b421-8f08c4e0cb18/bash.openssl.ca.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env -S bash -e
 #
-# OpenSSL certificate generator.
+# OpenSSL certificate generator with CA.
 #
 # @package    Bash
 # @author     Kai Kimera <mail@kai.kim>
 # @copyright  2023 Library Online
 # @license    MIT
-# @version    0.1.1
+# @version    0.1.2
 # @link       https://lib.onl/ru/articles/2023/10/6733cb51-62a0-5ed9-b421-8f08c4e0cb18/
 # -------------------------------------------------------------------------------------------------------------------- #
 
@@ -30,7 +30,7 @@ ca_key='ca.root.key'
 ca_crt='ca.root.crt'
 
 # Specifies the number of days to make a certificate valid for.
-# Default is 30 days.
+# Default is 10 years.
 days='3650'
 
 # Timestamp.
@@ -40,18 +40,21 @@ ts="$(( $( ${date} -u '+%s%N' ) / 1000000 ))"
 sfx=$( ${shuf} -i '1000-9999' -n 1 --random-source='/dev/random' )
 
 # -------------------------------------------------------------------------------------------------------------------- #
+# -----------------------------------------------------< SCRIPT >----------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# -------------------------------------------------------------------------------------------------------------------- #
 # CERTIFICATE AUTHORITY.
 # -------------------------------------------------------------------------------------------------------------------- #
 
 ca() {
   local ec='secp384r1'
 
-  echo "" && echo "--- [CREATE] Certificate Authority" && echo ""
+  echo "" && echo "--- [SSL] Certificate Authority" && echo ""
   ${ossl} req -x509 -newkey ec:<( openssl ecparam -name "${ec}" ) \
-    -nodes -days "${days}" \
-    -keyform 'PEM' -outform 'PEM' \
+    -nodes -sha384 -days "${days}" \
     -keyout "${ca_key}" -out "${ca_crt}" \
-    && ${ossl} x509 -in "${ca_crt}" -text
+    && ${ossl} x509 -in "${ca_crt}" -text -noout
 }
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -60,30 +63,29 @@ ca() {
 
 cert() {
   local ec='prime256v1'
-  local key_pvt="${ts}.${sfx}.key.private.pem"
-  local key_pub="${ts}.${sfx}.key.public.pem"
+  local key_pvt="${ts}.${sfx}.private.key"
+  local key_pub="${ts}.${sfx}.public.key"
   local csr="${ts}.${sfx}.csr"
   local crt="${ts}.${sfx}.crt"
   local p12="${ts}.${sfx}.p12"
   local srl="${ts}.${sfx}.srl"
 
-  echo "" && echo "--- [CREATE] Client Certificate [${ts}]" && echo ""
+  ! [[ -x "${ossl}" ]] && { echo >&2 "'openssl' is not installed!"; exit 1; }
+  ! [[ -x "${shuf}" ]] && { echo >&2 "'shuf' is not installed!"; exit 1; }
+
+  echo "" && echo "--- [SSL] Client Certificate [${ts}]" && echo ""
   ${ossl} ecparam -name "${ec}" -genkey -noout -out "${key_pvt}" \
     && ${ossl} ec -in "${key_pvt}" -pubout -out "${key_pub}" \
     && ${ossl} req -new -key "${key_pvt}" -out "${csr}" \
     && ${ossl} x509 -req -in "${csr}" \
         -CA "${ca_crt}" -CAkey "${ca_key}" -days "${days}" \
         -CAcreateserial -CAserial "${srl}" \
-        -outform 'PEM' -out "${crt}" \
+        -out "${crt}" \
     && ${ossl} pkcs12 -export \
         -inkey "${key_pvt}" \
         -in "${crt}" -out "${p12}" \
     && ${ossl} verify -CAfile "${ca_crt}" "${crt}" \
-    && ${ossl} x509 -in "${crt}" -text
+    && ${ossl} x509 -in "${crt}" -text -noout
 }
-
-# -------------------------------------------------------------------------------------------------------------------- #
-# -------------------------------------------------< RUNNING SCRIPT >------------------------------------------------- #
-# -------------------------------------------------------------------------------------------------------------------- #
 
 "$@"
---
+++ ru/articles/2023/10/6733cb51-62a0-5ed9-b421-8f08c4e0cb18/bash.openssl.ssc.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env -S bash -e
+#
+# OpenSSL self signed certificate generator.
+#
+# @package    Bash
+# @author     Kai Kimera <mail@kai.kim>
+# @copyright  2024 Library Online
+# @license    MIT
+# @version    0.1.0
+# @link       https://lib.onl/ru/articles/2023/10/6733cb51-62a0-5ed9-b421-8f08c4e0cb18/
+# -------------------------------------------------------------------------------------------------------------------- #
+
+(( EUID == 0 )) && { echo >&2 'This script should not be run as root!'; exit 1; }
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# CONFIGURATION.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Get 'openssl' command.
+ossl="$( command -v openssl )"
+
+# Specifies the number of days to make a certificate valid for.
+# Default is 10 years.
+days='3650'
+
+# The two-letter country code where your company is legally located.
+country='RU'
+
+# The state/province where your company is legally located.
+state='Russia'
+
+# The city where your company is legally located.
+city='Moscow'
+
+# Your company's legally registered name (e.g., YourCompany, Inc.).
+org='YourCompany'
+
+# The fully-qualified domain name (FQDN) (e.g., www.example.com).
+host="${1:-example.com}"
+
+# Your email address.
+email="mail@${host}"
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# -----------------------------------------------------< SCRIPT >----------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+! [[ -x "${ossl}" ]] && { echo >&2 "'openssl' is not installed!"; exit 1; }
+
+echo "" && echo "--- [SSL] Self Signed Certificate: '${host}'" && echo ""
+
+if [[ "${2}" = 'rsa' ]]; then
+  ${ossl} genrsa -out "${host}.key" 2048
+else
+  ${ossl} ecparam -genkey -name 'prime256v1' -out "${host}.key"
+fi
+
+if [[ -f "${host}.key" ]]; then
+  ${ossl} req -new -sha256 -key "${host}.key" -out "${host}.csr" \
+    -subj "/C=${country}/ST=${state}/L=${city}/O=${org}/emailAddress=${email}/CN=${host}" \
+    -addext "subjectAltName=DNS:${host},DNS:*.${host}" \
+    && ${ossl} req -x509 -sha256 -days ${days} \
+      -key "${host}.key" -in "${host}.csr" -out "${host}.crt" \
+    && ${ossl} x509 -in "${host}.crt" -text -noout
+else
+  echo "'${host}.key' not found!" && exit 1
+fi
+
+exit 0
