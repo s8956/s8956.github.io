76c80398681b6ec8da48251d43ba53ddd48a5e87
ghpbot <ghpbot@bk.ru>
2024-04-16 15:14:00 +0000

    deploy: s8956/site@d20273a5f16d4f51f0ae5e5cbe702abe97824e8d

--- js/hl.worker.js
+++ js/hl.worker.js
@@ -1,14 +1,16 @@
 onmessage = ($event) => {
   importScripts('/js/code.bundle.min.js');
-  const $result = [];
-  $event.data.forEach(($codeData) => {
-    let $language = $codeData.language;
-    if (self.hljs.getLanguage($language) === undefined) {
-      $language = 'plaintext';
-    }
-    $result.push(
-      self.hljs.highlight($codeData.code, {language: $language}).value
-    );
-  });
+  self.hljs.registerAliases(['rsc'], {languageName: 'routeros'});
+
+  const $len = $event.data.length;
+  let $result = [];
+
+  for (let $i = 0; $i < $len; ++$i) {
+    const $data = $event.data[$i];
+    const $lang = $data.language;
+    if (self.hljs.getLanguage($lang) === undefined) $lang = 'plaintext';
+    $result.push(self.hljs.highlight($data.code, {language: $lang}).value);
+  }
+
   postMessage($result);
-};
+}
