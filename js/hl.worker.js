14b3cca2e968b245f38e1874a06035be4de4ba97
ghpbot <ghpbot@bk.ru>
2024-04-16 05:46:54 +0000

    deploy: s8956/site@a156005bf2be3539e998277647d9389cfc696584

--- js/hl.worker.js
+++ js/hl.worker.js
@@ -1,5 +1,14 @@
-onmessage = (event) => {
+onmessage = ($event) => {
   importScripts('/js/code.bundle.min.js');
-  const result = self.hljs.highlightAuto(event.data);
-  postMessage(result.value);
+  const $result = [];
+  $event.data.forEach(($codeData) => {
+    let $language = $codeData.language;
+    if (self.hljs.getLanguage($language) === undefined) {
+      $language = 'plaintext';
+    }
+    $result.push(
+      self.hljs.highlight($codeData.code, {language: $language}).value
+    );
+  });
+  postMessage($result);
 };
