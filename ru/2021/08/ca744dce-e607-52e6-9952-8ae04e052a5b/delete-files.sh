b5182ad05965b9a9c7fdf7d76cebfabffbd6324d
ghpbot <ghpbot@bk.ru>
2025-03-24 20:13:35 +0000

    deploy: s8956/site@74f29da3c237a214e30d605895c2aa8ab78780d3

--- ru/2021/08/ca744dce-e607-52e6-9952-8ae04e052a5b/delete-files.sh
+++ ru/2021/08/ca744dce-e607-52e6-9952-8ae04e052a5b/delete-files.sh
@@ -13,4 +13,4 @@
 
 days="${1}"; dir="${2}"
 
-{ find "${dir}" -type f -mtime "+${days}" -print0 | xargs -0 rm -f; } && exit 0
+{ find "${dir}" -type f -mtime "+${days}" -print0 | xargs -0 rm -f --; } && exit 0
