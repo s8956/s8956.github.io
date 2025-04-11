e90be5428643dc65d64fab46ee5006b62ca13404
ghpbot <ghpbot@bk.ru>
2025-04-11 21:59:27 +0000

    deploy: s8956/site@52870c5773f847d2b637e032f309ab6aca8a550b

--- ru/2023/10/c6e32349-8fc2-53f9-9a42-790878a64076/pwsh.find.adl.ps1
+++
@@ -1,3 +0,0 @@
-function Find-AvailableDriveLetter() {
-  (68..90 | ForEach-Object { $L=[char]$_; if ((Get-PSDrive).Name -notContains $L) { $L } })[0]
-}
--- ru/2025/02/7deb49ab-bb4f-50e6-b196-82b4a9778a2d/irm.mariadb.drop.database.sql
+++
@@ -1,6 +0,0 @@
-drop database amavisd;
-drop database fail2ban;
-drop database iredadmin;
-drop database iredapd;
-drop database roundcubemail;
-drop database vmail;
